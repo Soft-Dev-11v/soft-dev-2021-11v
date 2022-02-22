da41c97eb6985b202dddf844bbf08b3d5ff0b5d9
Lacho <61554888+ZariProjects@users.noreply.github.com>
2022-02-22 18:22:17 +0200

    Create googleDrive.py

---
+++ Project/util/googleDrive.py
@@ -0,0 +1,14 @@
+def saveToDrive():
+    print("saved")
+
+
+def getFromDrive():
+    print("got from google drive")
+
+
+def deleteFromDrive():
+    print("deleted")
+
+
+def editToDrive():
+    print("edited")
