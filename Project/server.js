4a4d1852f094a2fc2af0f6b31e94366bd57899eb
0DakatA0 <dstoilov199@gmail.com>
2022-02-25 18:36:12 +0200

    server.js set up

---
+++ Project/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const colors = require("colors");
+const port = 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
