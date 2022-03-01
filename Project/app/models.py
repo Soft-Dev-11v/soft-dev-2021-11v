e97c7ff36710608c09cfd4114bb5b54285b5de59
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2022-03-01 18:17:53 +0200

    first ver db models

--- Project/app/models.py
+++ Project/app/models.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 """
 Copyright (c) 2019 - present AppSeed.us
 """
@@ -32,3 +31,54 @@ class Users(db.Model, UserMixin):
         db.session.commit( )
 
         return self 
+
+
+
+class Recipe():
+
+    __tablename__ = 'Recipes'
+    
+    name        = db.Column(db.String(50))
+    description = db.Column(db.String)
+    image       = db.Column(db.String(50))
+
+    def __init__(self, id, name, description, image):
+        self.id          = id
+        self.name        = name
+        self.description = description
+        self.image       = image
+
+    def save(self):
+        # inject self into db session    
+        db.session.add ( self )
+        # commit change and save the object
+        db.session.commit( )
+
+        return self
+
+class Ingredients():
+
+    __tablename__ = 'Ingredients'
+
+    id          = db.Column(db.Integer, primary_key=True)
+    name        = db.Column(db.String(50))
+    amount      = db.Column(db.Integer)
+    type        = db.Column(db.String())
+    recipeId    = db.Column(db.Integer)
+
+    def __init__(self, id, name, amount, type, recipeId):
+        self.id         = id
+        self.name       = name
+        self.amount     = amount
+        self.type       = type
+        self.recipeId   = recipeId
+
+    def save(self):
+
+        # inject self into db session    
+        db.session.add ( self )
+
+        # commit change and save the object
+        db.session.commit( )
+
+        return self
