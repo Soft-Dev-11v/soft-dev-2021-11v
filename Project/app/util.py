0faf12e8203706e065b037b2a2789fdc47ace4a2
Vladi Pavlov <vladi.pavlov26@gmail.com>
2022-03-14 13:37:19 +0200

    added image to recipie

--- Project/app/util.py
+++ Project/app/util.py
@@ -44,7 +44,9 @@ class Recipie:
     title = ""
     id = 0
     ingredients = []
+    image = ""
     def __init__(self, recipie):
+        self.image = recipie["image"]
         self.title = recipie["title"]
         self.id = recipie["id"]
         for i in recipie["usedIngredients"]:
